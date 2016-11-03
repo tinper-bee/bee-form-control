3540e2a61e50429e882aaa8dbe7ba28ad4250e1c
“liurhc” <liurhc@yonyou.com>
2016-11-03 15:03:28 +0800

    ver. 0.0.6

--- demo/index.js
+++ demo/index.js
@@ -1,3 +1,44 @@
-import Demo from './FormControlDemo';
+import FormControl from '../src';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(<Demo/>, document.getElementById('tinperBeeDemo'));
+
+
+function demo1() {
+    return (
+        <div className="demo-form-control">
+        	<FormControl placeholder="Enter text"/>			
+        </div>
+    );
+}
+
+class Demo2 extends Component {
+	constructor(props) {
+		super(props);
+		this.HanderChange = this.HanderChange.bind(this);
+		this.state = {
+			value: 'test'
+		}
+	}
+	HanderChange () {
+		let value = ReactDOM.findDOMNode(this.refs.demo2).value;
+		
+		this.setState ({value: value});
+	}
+	render(){
+		return( 
+			<div className="demo-form-control">
+
+				<FormControl ref='demo2' defaultValue='test' onChange={this.HanderChange} /> 
+
+				<span>{this.state.value}</span>
+            </div>
+        )
+	}
+
+}
+
+ReactDOM.render(demo1(), document.getElementById('ReactControlLabelDemo1'));
+ReactDOM.render(
+	<Demo2 />, 
+	document.getElementById('ReactControlLabelDemo2')
+);
