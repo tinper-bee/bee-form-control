c977055b04c9657fabfeae462237c7137ffc5d9e
“liurhc” <liurhc@yonyou.com>
2016-11-03 17:03:42 +0800

    new build

--- demo/index.js
+++ demo/index.js
@@ -11,34 +11,7 @@ function demo1() {
     );
 }
 
-class Demo2 extends Component {
-	constructor(props) {
-		super(props);
-		this.HanderChange = this.HanderChange.bind(this);
-		this.state = {
-			value: 'test'
-		}
-	}
-	HanderChange () {
-		let value = ReactDOM.findDOMNode(this.refs.demo2).value;
-		
-		this.setState ({value: value});
-	}
-	render(){
-		return( 
-			<div className="demo-form-control">
 
-				<FormControl ref='demo2' defaultValue='test' onChange={this.HanderChange} /> 
-
-				<span>{this.state.value}</span>
-            </div>
-        )
-	}
-
-}
 
 ReactDOM.render(demo1(), document.getElementById('ReactControlLabelDemo1'));
-ReactDOM.render(
-	<Demo2 />, 
-	document.getElementById('ReactControlLabelDemo2')
-);
+
