f0524dd7b34a63575e41db7dc964b42443f2b58e
yangchch6 <ycc54140127@163.com>
2020-07-20 19:13:08 +0800

    docs: api.md

--- demo/demolist/Demo10.js
+++ demo/demolist/Demo10.js
@@ -7,6 +7,12 @@ import React, {Component} from 'react';
 import FormControl from '../../src';
 
 export default class Demo10 extends Component {
+    onPressEnter = (e) => {
+        console.log('按下回车', e)
+    }
+    onResize = ({ width, height }) => {
+        console.log('onResize', width, height)
+    }
     render() {
         return (
             <div className="demo1">
@@ -20,6 +26,8 @@ export default class Demo10 extends Component {
                     componentClass="textarea"
                     placeholder="Controlled autosize"
                     autoSize={{ minRows: 3, maxRows: 5 }}
+                    onPressEnter={this.onPressEnter}
+                    onResize={this.onResize}
                 />
             </div>
         )
